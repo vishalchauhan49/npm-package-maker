44790b180342dfaf167aa8af0fc24f81e648b48b
Vishal <hackone710@gmail.com>
2026-07-14 16:26:35 +0530

    Npm Package made successfully

---
+++ math/index.js
@@ -0,0 +1,19 @@
+function add(num1 , num2){
+    return num1 + num2;
+}
+
+function sub(num1 , num2){
+    return num1 - num2;
+}
+
+function mul(num1 , num2){
+    return num1 * num2;
+}                   
+function div(num1 , num2){   
+    if(num2 === 0){
+        return "Error: Division by zero is not allowed.";
+    }
+    return num1 / num2;
+}
+
+export { add, sub, mul, div }; 
